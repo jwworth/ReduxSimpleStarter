f73c1c5d551bdf2337ff4e072d471c4429e45aa7
Jake Worth <jake@jakeworth.com>
2017-08-18 14:53:01 -0500

    Complete Video #45

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "JS the good parts" },
+    { title: "Harry Potter" },
+    { title: "The Dark Tower" },
+    { title: "Eloquent Ruby" }
+  ];
+}
